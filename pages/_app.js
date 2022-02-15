fc90ef0fb5ce4781baad5dceba99d78e7659b017
Imran Hossain <imran@algosolver.com>
2022-02-15 10:59:04 +0600

    init

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import {Provider} from 'react-redux'
+import Store from "../store";
+import '../styles/globals.css'
+
+
+function MyApp({ Component, pageProps }) {
+  return(
+      <Provider store={Store}>
+        <Component {...pageProps} />
+      </Provider>
+     )
+}
+
+export default MyApp
