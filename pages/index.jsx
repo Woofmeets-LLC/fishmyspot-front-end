8386e07ba2eba1650ad8b46266b3c6c239eb21c0
Habibullah Bahar Piash <habibullahbahar.piash@gmail.com>
2022-02-15 12:58:49 +0600

    Tailwind configured, fonts & colors added to tailwind, Folder structure modified.

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import HomeLayout from "../layouts/HomeLayout";
+
+const Home = () => {
+    return (
+        <HomeLayout>
+            <div className="container">
+                <div className="">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, harum molestiae? Quis, nulla? Odit dolores voluptates quaerat explicabo illo cum officia numquam commodi consequatur minima. Mollitia eveniet dignissimos nemo velit?
+                </div>
+            </div>
+        </HomeLayout>
+    );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,74 @@ module.exports = {
     "./layouts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      xs: '411px',
+      sm: '540px',
+      smd: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1400px',
+      '3xl': '1920px',
+    },
+    extend: {
+      fontFamily: {
+        "food-truck": ['food-truck', 'sans-serif'],
+        "trade-gothic": ['trade-gothic', 'sans-serif'],
+        "trade-gothic-bold": ['trade-gothic-bold', 'sans-serif'],
+      },
+      colors: {
+        primary: '#304E6B',
+        secondary: '#F7BE32',
+        "highlight-1": '#243746',
+        "highlight-2": '#65947E',
+        "highlight-3": '#7BAED4',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          padding: '0px 1rem',
+          '@screen xs': {
+            maxWidth: 'auto',
+            margin: '0px auto',
+          },
+          '@screen sm': {
+            maxWidth: '462px',
+            margin: '0px auto',
+          },
+
+          '@screen smd': {
+            maxWidth: '462px',
+            margin: '0px auto',
+          },
+          '@screen md': {
+            maxWidth: '750px',
+            margin: '0px auto',
+            padding: '0px auto',
+          },
+          '@screen lg': {
+            maxWidth: '970px',
+            margin: '0px auto',
+          },
+          '@screen xl': {
+            maxWidth: '1170px',
+            margin: '0px auto',
+          },
+
+          '@screen 2xl': {
+            maxWidth: '1250px',
+            margin: '0px auto',
+          },
+
+          '@screen 3xl': {
+            maxWidth: '1396px',
+            margin: '0px auto',
+          },
+        },
+      });
+    },
+  ],
 }
