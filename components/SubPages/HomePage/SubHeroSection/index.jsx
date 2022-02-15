4c9a5425491adcfdd324f0b2681644ea265e12fc
Habibullah Bahar Piash <habibullahbahar.piash@gmail.com>
2022-02-15 12:59:05 +0600

    Tailwind configured, fonts & colors added to tailwind, Folder structure modified.

---
+++ components/SubPages/HomePage/SubHeroSection/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SubHeroSection = () => {
+    return (
+        <div>
+            This is hero section
+        </div>
+    );
+};
+
+export default SubHeroSection;
