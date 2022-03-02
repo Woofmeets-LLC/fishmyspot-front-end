7cefd70cfab399fa251c694f57f90f787e6db306
Ankur Datta <ankur@algosolver.com>
2022-03-02 17:21:39 +0600

    feat: add redirect page for successful paypal onboarding

--- pages/onboarding/index.jsx
+++ pages/onboarding/index.jsx
@@ -9,9 +9,13 @@ const PaypalOnboardButton = () => {
 
   const getPayPalOnboardingLink = async () => {
     setLoadingState(true);
+
     try {
-      const res = await axios.get(
-        "http://localhost:5000/paypal/generate-signup-link"
+      const res = await axios.post(
+        "http://localhost:5000/paypal/generate-signup-link",
+        {
+          tracker_id: 1,
+        }
       );
       const data = res.data;
       console.log(data);
@@ -34,11 +38,27 @@ const PaypalOnboardButton = () => {
           key={"paypal-onboard-button"}
           className="bg-gradient-to-tr from-blue-500 to-blue-400 p-2 rounded-md text-white shadow-md w-max inline-flex gap-2"
         >
-          {!loading && (
+          {loading && (
             <svg
-              className="animate-spin h-5 w-5 mr-3  text-white"
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
               viewBox="0 0 24 24"
-            ></svg>
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
           )}
           Get Paypal onboarding URL
         </button>
