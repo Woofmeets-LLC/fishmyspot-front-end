3f05324aec00f5b3adabe92c5335dfc367c76cc7
Imran Hossain <imran@algosolver.com>
2022-03-07 17:34:25 +0600

    search filter done

--- store/index.js
+++ store/index.js
@@ -1,20 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
 import authSliceReducer from "./slices/authSlice";
 import listSpotContentsSliceReducer from "./slices/listSpotContentsSlice";
 import modalsSliceReducer from "./slices/modalsSlice";
+import autocompletetionSlice from "./slices/autocompletetionSlice";
 
 
 export const store = configureStore({
     reducer: {
-      modals: modalsSliceReducer,
-      auth: authSliceReducer,
-      listSpotContents: listSpotContentsSliceReducer,
+        modals: modalsSliceReducer,
+        auth: authSliceReducer,
+        listSpotContents: listSpotContentsSliceReducer,
+        place: autocompletetionSlice
     },
-    middleware: (getDefaultMiddleware)=>getDefaultMiddleware({
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
     }),
-  });
-  
+});
 
 
 export default store
