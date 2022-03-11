2f0d9a243f7bc1556b198675de42447f641ebec9
Habibullah Bahar Piash <habibullahbahar.piash@gmail.com>
2022-03-11 12:32:22 +0600

    Access to pond edit part completed

--- pages/edit-pond/access-to-pond/[pond-id].jsx
+++ pages/edit-pond/access-to-pond/[pond-id].jsx
@@ -2,18 +2,21 @@
 import { Form, Formik } from 'formik';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 import * as yup from 'yup';
 import EditPondContainer from '../../../components/SubPages/EditPondPage/EditPondContainer';
 import SubAccessToPondEdit from '../../../components/SubPages/EditPondPage/SubAccessToPondEdit';
 import HomeLayout from '../../../layouts/HomeLayout';
 import { listingImagesUpload } from '../../../services/listing-spot-data-organiging/listingImageUpload';
 import { getSdk } from '../../../sharetribe/sharetribeSDK';
+import { UUID } from '../../../types';
 
 const AccessToPondEdit = () => {
     const [pondData, setPondData] = useState({});
     const [loading, setLoading] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
-    const { query } = useRouter();
+    const { query, push } = useRouter();
     const getPondData = () => {
         setLoading(true)
         getSdk().ownListings.show({ id: query['pond-id'], include: ["images"], })
@@ -44,6 +47,48 @@ const AccessToPondEdit = () => {
         getPondData()
     }, [query]);
 
+    const handleUpdatePondData = async (values, helpers) => {
+        const existingImages = pondData?.relationships?.images?.data
+            ?.filter(image => values?.["ATP-images-base64"]?.includes(image?.attributes?.variants?.default?.url))
+            ?.map(image => image.id)
+
+        const imageFilesForUpload = values?.["ATP-images-file"]?.filter(photo => typeof (photo) != 'string');
+        const uploadedImages = await listingImagesUpload(imageFilesForUpload);
+        const newImagesIds = uploadedImages?.success ? uploadedImages?.uuids : [];
+
+        const requestData = {
+            images: [...existingImages, ...newImagesIds],
+            publicData: {
+                "access-to-pond-description": values?.["ATP-description"],
+                "additional-information-description": values?.["additional-information-description"],
+            }
+        }
+
+        setIsSubmitting(true);
+
+        const updatingPondData = getSdk().ownListings.update({
+            id: new UUID(query['pond-id']),
+            ...requestData,
+        })
+
+        toast.promise(updatingPondData, {
+            duration: 3000,
+            loading: 'Pond listing data updating...',
+            success: (res) => {
+                console.log({ "toast-res": res });
+                setTimeout(() => push("/own-spot-list"), 2000)
+                setIsSubmitting(false);
+                return `Your pond updated successfully!`
+            },
+            error: (err) => {
+                console.log(err);
+                setIsSubmitting(false);
+                return `Pond updating failed. Please try again!`;
+            },
+        })
+
+    }
+
     return (
         <HomeLayout>
             <EditPondContainer>
@@ -51,7 +96,7 @@ const AccessToPondEdit = () => {
                     enableReinitialize
                     initialValues={{
                         "ATP-description": pondData?.attributes?.publicData?.['access-to-pond-description'] || '',
-                        "ATP-images-file": [],
+                        "ATP-images-file": pondData?.relationships?.images?.data?.map(image => image?.attributes?.variants?.default?.url) || [],
                         "ATP-images-base64": pondData?.relationships?.images?.data?.map(image => image?.attributes?.variants?.default?.url) || [],
                         "additional-information-description": pondData?.attributes?.publicData?.['additional-information-description'] || '',
                     }}
@@ -61,24 +106,32 @@ const AccessToPondEdit = () => {
                             "ATP-images-base64": yup.array().min(2, 'You must upload at least 2 photos!'),
                         })
                     }
-                    onSubmit={async (values, helpers) => {
-                        const existingImages = pondData?.relationships?.images?.data
-                            ?.filter(image => values?.["ATP-images-base64"]?.includes(image?.attributes?.variants?.default?.url))
-                            ?.map(image => image.id)
-
-                        const uploadedImages = await listingImagesUpload(values?.["ATP-images-file"]);
-                        const newImagesIds = uploadedImages;
-                        const requestData = {
-                            images: [...existingImages],
-                        }
-                        console.log({ requestData, newImagesIds, files: values?.["ATP-images-file"] });
-
-                    }}>
+                    onSubmit={handleUpdatePondData}>
                     <Form>
                         {
                             loading
                                 ? <div>Loading...</div>
-                                : <SubAccessToPondEdit />
+                                : <>
+                                    <SubAccessToPondEdit />
+                                    <div className="my-2 text-right">
+                                        <button
+                                            type={isSubmitting ? "button" : "submit"}
+                                            className="flex justify-center items-center bg-secondary text-white font-trade-gothic-bold rounded py-2 px-8 ml-auto">
+                                            {
+                                                isSubmitting &&
+                                                <span className="animate-spin flex justify-center items-center w-7">
+                                                    <span className="rounded-full h-5 w-5 border-t-2 border-b-2 border-white"></span>
+                                                </span>
+                                            }
+                                            {
+                                                isSubmitting
+                                                    ? "Loading..."
+                                                    : `Update`
+                                            }
+
+                                        </button>
+                                    </div>
+                                </>
                         }
                     </Form>
                 </Formik>
