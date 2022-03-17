2c8fe9c9fa31d0e798a3ad54701e4c3480a1c223
sanjoypaul <sanjoypp@gmail.com>
2022-03-17 17:26:21 +0600

    messaging functionality

--- components/SubPages/MessagePage/SubBody/Message/Message.jsx
+++ components/SubPages/MessagePage/SubBody/Message/Message.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Message = ({ message, sentfrom }) => {
+const Message = ({ message, sentfrom, createdAt }) => {
   return (
     <div className="flex items-center">
       <div className={`max-w-[180px] sm:max-w-[300px] xl:max-w-[472px] mb-2 ${sentfrom ? 'order-1' : ''}`}>
         <div className={`w-fit ${sentfrom ? 'bg-[#efefef] text-highlight-1 rounded-br-2xl rounded-tl-xl rounded-bl-xl ml-2' : 'bg-highlight-2 text-white rounded-bl-2xl rounded-tr-xl rounded-br-xl mr-2'} text-sm `}>
-          <p className='px-3 py-2 sm:px-4 md:px-4 2xl:p-4 3xl:py-5 3xl:px-[18px]'>{message?.inputValue}</p>
+          <p className='px-3 py-2 sm:px-4 md:px-4 2xl:p-4 3xl:py-5 3xl:px-[18px]'>{message}</p>
         </div>
       </div>
       <div className={`text-[8px] sm:text-xs lg:text-sm text-highlight-1 font-trade-gothic ${sentfrom && "ml-auto text-right"}`}>
-        {message?.time}
+        {createdAt}
       </div>
     </div>
   );
--- components/SubPages/MessagePage/SubSidebar/index.jsx
+++ components/SubPages/MessagePage/SubSidebar/index.jsx
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
 import MessageCard from '../MessageCard/MessageCard';
 
-const SubSidebar = () => {
-  const [isActive, setIsActive] = useState(0);
+const SubSidebar = ({ isActive, setIsActive, transactionIds, includedListingData, includedMessageData }) => {
   return (
     <div className='p-2 md:pr-3 h-[440px] md:h-[605px] border-r border-r-gray-300 overflow-y-auto lg:pt-7 message__scrollbar'>
       {
-        [...Array(10).keys()].map((dt, index) => {
-          return (
-            <MessageCard
-              key={index}
-              id={index}
-              isActive={isActive}
-              setIsActive={setIsActive}
-            />
-          )
+        // transactionIds.forEach(id => {
+        //   console.log({ [id]: { listing: includedListingData[id], message: includedMessageData[id] } });
+        // });
+
+        transactionIds.map((id, index) => {
+          if (includedMessageData[id] && includedListingData[id] && includedMessageData[id]?.data[0]?.attributes) {
+            const { content, createdAt } = includedMessageData[id]?.data[0]?.attributes;
+            return (
+              <MessageCard
+                key={index}
+                id={id}
+                isActive={isActive}
+                setIsActive={setIsActive}
+                listingTitle={includedListingData[id].attributes.title}
+                lastMessage={content}
+                lastMessageDate={new Date(createdAt)}
+              />
+            )
+          }
         })
       }
     </div>
