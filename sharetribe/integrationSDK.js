0caa15da83901971e6e7ba2c647e1e6e0a2172f4
Habibullah Bahar Piash <habibullahbahar.piash@gmail.com>
2022-03-17 18:07:27 +0600

    Merged with dev

--- sharetribe/integrationSDK.js
+++ sharetribe/integrationSDK.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const {createInstance} = require('sharetribe-flex-integration-sdk');
 
 
@@ -6,11 +5,3 @@ export const integrationSdk = createInstance({
     clientId: process.env.FLEX_INTEGRATION_API_CLIENT_ID,
     clientSecret: process.env.FLEX_INTEGRATION_API_SECRET_KEY
 });
-=======
-const { createInstance } = require('sharetribe-flex-integration-sdk');
-
-export const integrationSdk = createInstance({
-  clientId: process.env.FLEX_INTEGRATION_API_CLIENT_ID,
-  clientSecret: process.env.FLEX_INTEGRATION_API_SECRET_KEY,
-});
->>>>>>> 34faf2f326a4a9ba4c84a8522ac5ccdd389dab11
