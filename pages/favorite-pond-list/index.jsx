167bb8f5d6b406ec54653179907743394da184e8
sanjoypaul <sanjoypp@gmail.com>
2022-03-24 16:21:56 +0600

    favorite listing spot page and fixing bug issue and sdk implementation

--- pages/favorite-pond-list/index.jsx
+++ pages/favorite-pond-list/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import SubServices from '../../components/SubPages/ServicesPage/SubServicesList';
 import { useCurrentUser } from '../../hooks/users';
