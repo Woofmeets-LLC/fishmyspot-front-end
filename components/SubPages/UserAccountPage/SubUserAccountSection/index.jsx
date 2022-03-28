cbb53f8022cb554bacea4145a8072fb5965c07a4
sanjoypaul <sanjoypp@gmail.com>
2022-03-28 16:47:40 +0600

    message page updated

--- components/SubPages/UserAccountPage/SubUserAccountSection/index.jsx
+++ components/SubPages/UserAccountPage/SubUserAccountSection/index.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 import { PageHeader } from '../../../Common';
 import ChangePassword from '../ChangePassword';
 import PersonalInfo from '../PersonalInfo';
 import UserAccountSectionTitle from '../UserAccountSectionTitle';
 
 const SubUserAccountSection = () => {
+  const { user } = useSelector(state => state.auth);
   return (
     <div className='pb-12 sm:pb-16 md:pb-20 lg:pb-28 2xl:pb-32'>
       <div className="text-primary space-y-2 md:space-y-3 lg:space-y-5 mb-5 md:mb-6 lg:mb-10 pt-6 sm:pt-12 lg:pt-16 2xl:pt-20">
         <PageHeader
           title={"Account"}
-          userName={"Larissa Smith"}
-          userEmail={"larissa@gmail.com"}
+          userName={user?.profile?.firstName + " " + user?.profile?.lastName}
+          userEmail={user?.email}
         />
       </div>
       <UserAccountSectionTitle title={"Personal Info"} />
