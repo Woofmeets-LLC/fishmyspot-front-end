872711ca63f66539dab878e7fed3ef3e50878884
Biswajit Das <biswajit@algosolver.com>
2022-06-08 11:37:20 +0600

    feat: FISH-27 add api for gift card payment intent

---
+++ pages/api/giftcard/buy-card.js
@@ -0,0 +1,31 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
+export default async function handler(req, res) {
+  //create gift card payment intent
+
+  if (req.method === 'POST') {
+    const { amount, receipt_email, description, user_id } = req.body;
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount * 100,
+      currency: 'usd',
+      payment_method_types: ['card'],
+      receipt_email,
+      description,
+      metadata: {
+        user_id,
+      },
+    });
+
+    //return payment intent details
+    res.status(200).json({
+      client_secret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(400).json('Not found');
+  }
+}
