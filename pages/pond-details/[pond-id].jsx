e1955856ecf2edbdd649ee74269ba0683cb3c3b4
sanjoy paul <sanjoy@algosolver.com>
2022-06-14 14:59:02 +0600

    feat: FISH-27 add gift card redemtion for pond reservation

--- pages/pond-details/[pond-id].jsx
+++ pages/pond-details/[pond-id].jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/display-name */
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { ClipLoader } from 'react-spinners';
 import SubReservationSection from '../../components/SubPages/ServiceListPage/SubReservationSection';
@@ -16,19 +16,23 @@ const PondDetails = () => {
   const [pondImages, setPondImages] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const { isLoggedIn, user } = useSelector(state => state.auth);
+  const { isLoggedIn, user } = useSelector((state) => state.auth);
 
-  const { query } = useRouter()
+  const { query } = useRouter();
 
   useEffect(() => {
     setLoading(true);
-    getSdk().listings.show({
-      id: query['pond-id'],
-      include: ['images', 'author']
-    }, {
-      expand: true,
-    })
-      .then(res => {
+    getSdk()
+      .listings.show(
+        {
+          id: query['pond-id'],
+          include: ['images', 'author'],
+        },
+        {
+          expand: true,
+        }
+      )
+      .then((res) => {
         setLoading(false);
 
         const data = res?.data?.data;
@@ -38,79 +42,82 @@ const PondDetails = () => {
           relationships: {
             ...data.relationships,
             images: {
-              data: data?.relationships?.images?.data?.map(image => {
-                return included?.find(included => included.id.uuid === image.id.uuid);
-              })
-            }
-          }
-        }
-        setPondImages(formattedData?.relationships?.images?.data?.map(image => image?.attributes?.variants?.default?.url));
-        setPondData(formattedData)
+              data: data?.relationships?.images?.data?.map((image) => {
+                return included?.find(
+                  (included) => included.id.uuid === image.id.uuid
+                );
+              }),
+            },
+          },
+        };
+        setPondImages(
+          formattedData?.relationships?.images?.data?.map(
+            (image) => image?.attributes?.variants?.default?.url
+          )
+        );
+        setPondData(formattedData);
       })
-      .catch(err => {
+      .catch((err) => {
         setLoading(false);
-      })
-  }, [query])
+      });
+  }, [query]);
 
   return (
     <HomeLayout
       ogTags={{
         title: pondData?.attributes?.title,
         description: pondData?.attributes?.description,
-        image: pondImages?.[0]
+        image: pondImages?.[0],
       }}
     >
-      {
-        loading
-          ? <div className="flex justify-center items-center flex-wrap my-10">
-            <ClipLoader size={50} color={'#1971ff'} />
-            <h2 className="w-full text-center font-semibold mt-2">Loading...</h2>
-          </div>
-          : <>
-            <ViewImageModal
-              pondImages={pondImages}
-              imageModal={imageModal}
-              setImageModal={setImageModal}
-            />
+      {loading ? (
+        <div className="my-10 flex flex-wrap items-center justify-center">
+          <ClipLoader size={50} color={'#1971ff'} />
+          <h2 className="mt-2 w-full text-center font-semibold">Loading...</h2>
+        </div>
+      ) : (
+        <>
+          <ViewImageModal
+            pondImages={pondImages}
+            imageModal={imageModal}
+            setImageModal={setImageModal}
+          />
 
-            <div className="bg-[#fbfbfb]">
-              <div className="container mt-6 md:mt-9 xl:mt-11">
-                {/* hero section */}
-                <SubServicesListHeroSection
-                  setImageModal={setImageModal}
-                  pondImages={pondImages}
-                />
+          <div className="bg-[#fbfbfb]">
+            <div className="container mt-6 md:mt-9 xl:mt-11">
+              {/* hero section */}
+              <SubServicesListHeroSection
+                setImageModal={setImageModal}
+                pondImages={pondImages}
+              />
 
-                {/* content section */}
-                <div className="flex flex-col lg:flex-row lg:justify-between gap-16 xl:gap-28 2xl:gap-[100px] 3xl:gap-[136px] mb-7 md:mb-10 lg:mb-16 xl:mb-20">
-                  <SubServicesDetailsSection pondData={pondData} />
-                  {
-                    (isLoggedIn &&
-                      user?.profile?.publicData?.account_type == 'angler')
-                      ? <SubReservationSection pondData={pondData} />
-                      : <div className="order-1 lg:order-2 w-full md:w-2/3 lg:w-[420px] 2xl:w-[510px] ">
-                        <div className="mx-auto shadow-lg rounded-lg bg-white">
-                          <div className='px-4 py-6 sm:px-7 sm:pt-8 sm:pb-10'>
-                            <h2 className="text-center font-trade-gothic-bold">
-                              {
-                                !isLoggedIn
-                                  ? "Log in to book the spot."
-                                  : "As you are a pond owner, you can not book any pond."
-                              }
-                            </h2>
-                          </div>
-                        </div>
+              {/* content section */}
+              <div className="flex flex-col gap-16 pb-7 md:pb-10 lg:flex-row lg:justify-between lg:pb-16 xl:gap-28 xl:pb-20 2xl:gap-[100px] 3xl:gap-[136px]">
+                <SubServicesDetailsSection pondData={pondData} />
+                {isLoggedIn &&
+                user?.profile?.publicData?.account_type == 'angler' ? (
+                  <SubReservationSection pondData={pondData} />
+                ) : (
+                  <div className="order-1 w-full md:w-2/3 lg:order-2 lg:w-[420px] 2xl:w-[510px] ">
+                    <div className="mx-auto rounded-lg bg-white shadow-lg">
+                      <div className="px-4 py-6 sm:px-7 sm:pt-8 sm:pb-10">
+                        <h2 className="text-center font-trade-gothic-bold">
+                          {!isLoggedIn
+                            ? 'Log in to book the spot.'
+                            : 'As you are a pond owner, you can not book any pond.'}
+                        </h2>
                       </div>
-                  }
-                </div>
-
-                {/* map */}
-                {/* <SubMapSection /> */}
-
+                    </div>
+                  </div>
+                )}
               </div>
+
+              {/* map */}
+              {/* <SubMapSection /> */}
             </div>
-          </>
-      }
+          </div>
+        </>
+      )}
     </HomeLayout>
   );
 };
