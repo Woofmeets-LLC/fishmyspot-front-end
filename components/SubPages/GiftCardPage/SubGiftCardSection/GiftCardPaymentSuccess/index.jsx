76202476fa52282779f933d123a3fdb77c20ccc6
sanjoy paul <sanjoy@algosolver.com>
2022-06-24 09:46:10 +0600

    fix: FISH-27 handle gitfcard payment success and failure message

--- components/SubPages/GiftCardPage/SubGiftCardSection/GiftCardPaymentSuccess/index.jsx
+++ components/SubPages/GiftCardPage/SubGiftCardSection/GiftCardPaymentSuccess/index.jsx
@@ -1,12 +1,15 @@
 import { useSelector } from 'react-redux';
 
 const GiftCardPaymentSuccess = () => {
-  const { user } = useSelector((state) => state.auth);
+  const { auth, giftCardData } = useSelector((state) => state);
   return (
     <div className="pt-6 text-center md:pt-10 xl:pt-20">
       <h1 className="mb-4 font-trade-gothic text-lg text-highlight-1 md:text-2xl lg:text-3xl xl:mb-6 xl:text-4xl 2xl:mb-8 2xl:text-5xl">
         Thank you,{' '}
-        {user && `${user?.profile?.firstName} ${user?.profile?.lastName}`}!
+        {auth?.user?.email
+          ? `${auth?.user?.profile?.firstName} ${auth?.user?.profile?.lastName}`
+          : giftCardData?.name}
+        !
       </h1>
       <h1 className="mb-5 font-food-truck text-xl uppercase text-primary underline md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
         For Purchasing Gift Card
