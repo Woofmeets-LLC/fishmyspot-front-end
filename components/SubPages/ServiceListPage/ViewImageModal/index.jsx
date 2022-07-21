c863d60f6fc7cea947bdd7ec737514b9efe792f8
Biswajit Das <biswajit@algosolver.com>
2022-07-21 13:23:52 +0600

    fix: SUP-1 fix Images do no fit slideshow

--- components/SubPages/ServiceListPage/ViewImageModal/index.jsx
+++ components/SubPages/ServiceListPage/ViewImageModal/index.jsx
@@ -1,13 +1,13 @@
 import { enableBodyScroll } from 'body-scroll-lock';
 import React from 'react';
-import Slider from "react-slick";
+import Slider from 'react-slick';
 import { Modal } from '../../../Common';
 
 const ViewImageModal = ({ pondImages, imageModal, setImageModal }) => {
   const handleClose = () => {
     setImageModal(false);
     enableBodyScroll(document?.body);
-  }
+  };
   const settings = {
     dots: false,
     infinite: true,
@@ -22,21 +22,27 @@ const ViewImageModal = ({ pondImages, imageModal, setImageModal }) => {
       onClose={handleClose}
       rounded={4}
     >
-      <div className='pond-details-slider w-[300px] sm:w-[360px] md:w-[600px] lg:w-[700px] xl:w-[800px] 2xl:w-[900px] 3xl:w-[1100px] h-[200px] sm:h-[230px] md:h-[360px] lg:h-[400px] xl:h-[450px] 2xl:h-[480px] 3xl:h-[600px] view-image-modal relative'>
+      <div className="pond-details-slider view-image-modal relative h-[200px] w-[300px] sm:h-[230px] sm:w-[360px] md:h-[360px] md:w-[600px] lg:h-[400px] lg:w-[700px] xl:h-[450px] xl:w-[800px] 2xl:h-[480px] 2xl:w-[900px] 3xl:h-[600px] 3xl:w-[1100px]">
         <Slider {...settings}>
-          {
-            pondImages?.map((image, index) => (
-              <div key={index} className='w-[300px] sm:w-[360px] md:w-[600px] lg:w-[700px] xl:w-[800px] 2xl:w-[900px] 3xl:w-[1100px] h-[200px] sm:h-[230px] md:h-[360px] lg:h-[400px] xl:h-[450px] 2xl:h-[480px] 3xl:h-[600px] relative'>
-                <img src={image} alt="pond" className='w-full h-full object-cover' />
-              </div>
-            ))
-          }
+          {pondImages?.map((image, index) => (
+            <div
+              key={index}
+              className="relative h-[200px] w-[300px] bg-gray-100 sm:h-[230px] sm:w-[360px] md:h-[360px] md:w-[600px] lg:h-[400px] lg:w-[700px] xl:h-[450px] xl:w-[800px] 2xl:h-[480px] 2xl:w-[900px] 3xl:h-[600px] 3xl:w-[1100px]"
+            >
+              <img
+                src={image}
+                alt="pond"
+                className="h-full w-full object-contain"
+              />
+            </div>
+          ))}
         </Slider>
-        <div className='absolute top-0 right-0'>
+        <div className="absolute top-0 right-0">
           <span
             onClick={() => handleClose()}
-            className='bg-black bg-opacity-50 cursor-pointer px-3 py-1'>
-            <span className='text-white text-sm lg:text-base'>Close</span>
+            className="cursor-pointer bg-black bg-opacity-50 px-3 py-1"
+          >
+            <span className="text-sm text-white lg:text-base">Close</span>
           </span>
         </div>
       </div>
