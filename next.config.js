510993280bb67e9b46892c2111e7cc7019b34b9e
Biswajit Das <biswajit@algosolver.com>
2022-08-10 10:38:47 +0600

    fix: PROJ-107 fix stripe live secret key

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     FLEX_MARKETPLACE_API_CLIENT_ID: '1f044608-70b7-46a6-a4eb-1927b7757d34',
     FLEX_MARKETPLACE_API_SECRET_KEY: '9ad0ba47bd933f4f1d6fc61fb4af5b337c554b90',
     GOOGLE_MAP_API_KEY: 'AIzaSyCfhL0D8h89t_m4xilQ-Nb8rlVpzXqAjdo',
-    STRIPE_SECRET_KEY: 'isokay',
+    STRIPE_SECRET_KEY: 'sk_live_pv3IsAnFTk8Zo8htmjEGKNOq',
     STRIPE_PUBLISHABLE_KEY: 'pk_live_SwWuArAnBV4Qf7dUETKjgMNw',
     NEXT_STRIPE_PUBLISHABLE_KEY: 'pk_live_SwWuArAnBV4Qf7dUETKjgMNw',
     BACKEND_URL: 'https://fish-my-spot-backend-op74rtdzqa-uc.a.run.app',
