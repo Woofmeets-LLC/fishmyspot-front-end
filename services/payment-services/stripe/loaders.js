c725c468778106610f3c84270c2710fb0a869db7
Biswajit Das <biswajit@algosolver.com>
2022-09-02 08:15:52 +0000

    Dev

--- services/payment-services/stripe/loaders.js
+++ services/payment-services/stripe/loaders.js
@@ -1,14 +1,14 @@
-import Stripe from "stripe";
+import Stripe from 'stripe';
 
 /**
  *
  * @returns {Stripe}
  */
 export const getStripeClientWithPublishableKey = () => {
-  const publishableKey = process.env.STRIPE_PUBLISHABLE_KEY;
+  const publishableKey = process.env.NEXT_STRIPE_PUBLISHABLE_KEY;
 
   return new Stripe(publishableKey, {
-    apiVersion: "2020-08-27",
+    apiVersion: '2020-08-27',
   });
 };
 
@@ -16,8 +16,8 @@ export const getStripeClientWithPublishableKey = () => {
  * @returns {Stripe}
  */
 export const getStripeClientWithSecretKey = () => {
-  const secretKey = process.env.STRIPE_SECRET_KEY;
+  const secretKey = process.env.NEXT_STRIPE_PUBLISHABLE_KEY;
   return new Stripe(secretKey, {
-    apiVersion: "2020-08-27",
+    apiVersion: '2020-08-27',
   });
 };
