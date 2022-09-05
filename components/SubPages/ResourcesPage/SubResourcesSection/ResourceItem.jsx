3181f41a3000b4a4b43e03c177995b026df10dbc
Biswajit Das <biswajit@algosolver.com>
2022-09-05 09:45:11 +0000

    Sup 78

--- components/SubPages/ResourcesPage/SubResourcesSection/ResourceItem.jsx
+++ components/SubPages/ResourcesPage/SubResourcesSection/ResourceItem.jsx
@@ -5,9 +5,7 @@ const ResourceItem = ({ title, href }) => {
     <Link href={`/resources/${href}`}>
       <a target="_blank" rel="noopener noreferrer">
         <h2 className="mb-3 flex items-center justify-start font-food-truck text-2xl text-primary">
-          <span className="mr-3 inline-block h-4 w-4 rounded-full bg-primary ">
-            {' '}
-          </span>
+          <span className="mr-3 inline-block h-4 w-4 rounded-full bg-primary "></span>
           <span> {title}</span>
         </h2>
       </a>
