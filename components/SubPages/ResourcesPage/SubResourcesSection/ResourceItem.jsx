ad3843d3a50dd97ae39fd44b8929facf8a07c336
Biswajit Das <biswajit@algosolver.com>
2022-09-05 09:50:11 +0000

    Dev

--- components/SubPages/ResourcesPage/SubResourcesSection/ResourceItem.jsx
+++ components/SubPages/ResourcesPage/SubResourcesSection/ResourceItem.jsx
@@ -4,9 +4,9 @@ const ResourceItem = ({ title, href }) => {
   return (
     <Link href={`/resources/${href}`}>
       <a target="_blank" rel="noopener noreferrer">
-        <h2 className="mb-3 font-food-truck text-2xl text-blue-400">
-          <span className="mr-3 inline-block h-4 w-4 rounded-full bg-blue-400"></span>
-          {title}
+        <h2 className="mb-3 flex items-center justify-start font-food-truck text-2xl text-primary">
+          <span className="mr-3 inline-block h-4 w-4 rounded-full bg-primary "></span>
+          <span> {title}</span>
         </h2>
       </a>
     </Link>
